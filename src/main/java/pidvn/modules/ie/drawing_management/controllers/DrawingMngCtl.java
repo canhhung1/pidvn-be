ccf4097f74d0c14285a7747e1702f84f776f884a
Canh Hung <canhhung.nguyen1@outlook.com>
2024-07-06 22:59:16 +0700

    coding gui email

--- src/main/java/pidvn/modules/ie/drawing_management/controllers/DrawingMngCtl.java
+++ src/main/java/pidvn/modules/ie/drawing_management/controllers/DrawingMngCtl.java
@@ -3,16 +3,22 @@ package pidvn.modules.ie.drawing_management.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pidvn.config.mail.DataMailDTO;
+import pidvn.config.mail.MailService;
 import pidvn.modules.ie.drawing_management.models.SearchVo;
 import pidvn.modules.ie.drawing_management.services.DrawingMngSvc;
+
+import javax.mail.MessagingException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("IE/DrawingManagement")
 public class DrawingMngCtl {
+
+    @Autowired
+    private MailService mailService;
 
 
     @Autowired
@@ -23,7 +29,31 @@ public class DrawingMngCtl {
         return new ResponseEntity<>(this.drawingMngSvc.getProjects(searchVo), HttpStatus.OK);
     }
 
+    @GetMapping("Mail")
+    public ResponseEntity<?> testEmail() {
+        Map result = new HashMap();
+        try {
+            DataMailDTO dataMail = new DataMailDTO();
+            dataMail.setTo("canhhung.nguyen1@gmail.com");
+            dataMail.setSubject("Test Send Email Spring boot");
 
+            Map<String, Object> props = new HashMap<>();
+            props.put("name", "Nguyễn Cảnh Hưng");
+            props.put("username", "hungnc225.it");
+            props.put("password", "pwd@123");
+            dataMail.setProps(props);
+            mailService.sendHtmlMail(dataMail, "client");
+
+            result.put("result","Send OK");
+        } catch (MessagingException e) {
+            result.put("result","Send ERROR");
+            result.put("error",e.toString());
+            throw new RuntimeException(e);
+        }
+
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
 
 }

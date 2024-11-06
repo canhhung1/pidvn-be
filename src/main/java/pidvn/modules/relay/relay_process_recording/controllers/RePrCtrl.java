a7028d00bd9e4f079a4e38ab6a751fdc30def493
Canh Hung <canhhung.nguyen1@outlook.com>
2024-11-06 23:11:45 +0700

    coding

---
+++ src/main/java/pidvn/modules/relay/relay_process_recording/controllers/RePrCtrl.java
@@ -0,0 +1,10 @@
+package pidvn.modules.relay.relay_process_recording.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Relay/ProcessRecording")
+public class RePrCtrl {
+
+}
